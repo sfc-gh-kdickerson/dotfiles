c79fcb7fdb3d971e6b5e35cd5deeeeef7e0ee87d
Kaleb Dickerson <kaleb.dickerson@snowflake.com>
2025-11-05 16:44:42 -0800

    update copilot

--- .config/nvim/lua/kaleb/plugins/copilot.lua
+++ .config/nvim/lua/kaleb/plugins/copilot.lua
@@ -3,13 +3,4 @@ return {
     "github/copilot.vim",
     enabled = true,
   },
-  {
-    "CopilotC-Nvim/CopilotChat.nvim",
-    enabled = false,
-    dependencies = {
-      { "nvim-lua/plenary.nvim", branch = "master" },
-    },
-    build = "make tiktoken",
-    opts = {},
-  },
 }
